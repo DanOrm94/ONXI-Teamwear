ee802e04381009191847bbcc1870ad665e445c89
DanOrm94 <142906041+DanOrm94@users.noreply.github.com>
2026-09-09 18:19:07 +0100

    Route club slugs to dedicated storefronts

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import ClubShop from './ClubShop'
+import { clubs } from './clubs'
 import './styles.css'
 import './hero.css'
 
+const slug = window.location.pathname.replace(/^\/+|\/+$/g, '').toLowerCase()
+const club = clubs[slug]
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {club ? <ClubShop club={club} /> : <App />}
   </StrictMode>,
 )
